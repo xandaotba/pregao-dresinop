c165426483ccfd4965d372b07a236ed839d02b37
Alexandre dos Santos <alexandre_tba@hotmail.com>
2025-05-05 16:38:23 -0400

    Criado as pastas 'models', 'controllers' e 'views', adicionado modelo ER do Banco.

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Pregão DRE de Sinop</title>
+    </head>
+    
+    <body>
+        <<header>
+            
+        </header>
+    </body>
+</html>
