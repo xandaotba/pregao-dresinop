394fce024e490d06f5d64829079153720b8e6858
Alexandre dos Santos <alexandre_tba@hotmail.com>
2025-05-15 17:37:57 -0400

    Criado estilo.css, enviado logo do site, criado o cabeçalho do site no index.php

--- index.php
+++ index.php
@@ -1,25 +1,39 @@
-<?php
-    require_once ('config.php');
-
-?>
-
-
 <!DOCTYPE html>
-<!--
-
--->
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <title>Pregão DRE de Sinop</title>
-        <link rel="stylesheet" href="css/estilo.css"/>
-    </head>
-    <body>
-        <?php
-       echo "Teste teste"
-        . "";
-       
-       $teste = new Fornecedores();
-        ?>
-    </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pregão DRE de Sinop</title>
+  <link rel="stylesheet" href="css/estilo.css?v=1.0">
+
+</head>
+<body>
+
+  <header>
+           <div class="texto-header">
+      Sistema Auxiliar de Pregão Presencial da DRE de Sinop
+    </div>
+  <div class="top-header">
+    <div class="logo-left">
+      <img src="images/Logotipo.png" alt="Logo esquerda">
+    </div>
+    <nav class="menu-central">
+      <ul>
+        <li><a href="#">Início</a></li>
+        <li><a href="#">Fornecedores</a></li>
+        <li><a href="#">Escolas</a></li>
+        <li><a href="#">Certames</a></li>
+        <li><a href="#">Municípios</a></li>
+        <li><a href="#">Produtos</a></li>
+      </ul>
+    </nav>
+    
+    <div class="logo-right">
+      <img src="images/Logo DRE.png" alt="Logo direita">
+    </div>
+  </div>
+</header>
+
+
+</body>
 </html>
